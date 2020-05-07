b25717cadd83e3df9c37f764668c37d64c13b888
ConorMaley <cmaley17@wooster.edu>
2020-05-06 22:20:54 -0400

    integrating with google sheets

--- calculateRanking.py
+++ calculateRanking.py
@@ -1,45 +1,9 @@
-import csv
-import sys
-import math
+import googleSheetsApi
+import eloCalculator
 
-def Probability(rating1, rating2):
-	return 1.0 * 1.0 / (1 + 1.0 * math.pow(10, 1.0 * (rating1 - rating2) / 400))
+def main():
+	masterSheet = googleSheetsApi.getMasterSheet()
 
-def CalcEloChange(PlayerELO, OppELO, ScoreConstant, Status):
-	# Pb = Probability(PlayerELO, OppELO)
-	Pa = Probability(OppELO, PlayerELO)
-
-	# # Player Wins
-	# if (Status == 1) : 
-	# 	PlayerELO = PlayerELO + ScoreConstant * (1 - Pa) 
-	# 	OppELO = OppELO + ScoreConstant * (0 - Pb)
-
-	# # Tie
-	# elif (Status == 0.5) :
-	# 	PlayerELO = PlayerELO + ScoreConstant * (0.5 - Pa) 
-	# 	OppELO = OppELO + ScoreConstant * (0.5 - Pb)
-
-	# # Opp wins
-	# else : 
-	# 	PlayerELO = PlayerELO + ScoreConstant * (0 - Pa) 
-	# 	OppELO = OppELO + ScoreConstant * (1 - Pb)
-
-	# return round(PlayerELO, 2)
-
-
-	if (Status == 1) : 
-		return ScoreConstant * (1 - Pa) 
-
-	# Tie
-	elif (Status == 0.5) :
-		return ScoreConstant * (0.5 - Pa) 
-
-	# Opp wins
-	else : 
-		return ScoreConstant * (0 - Pa) 
-
-with open(sys.argv[1]) as input_file:
-	csv_reader = csv.reader(input_file, delimiter=',')
 	Elos = {}
 	
 	playersArray = []
@@ -48,7 +12,7 @@ with open(sys.argv[1]) as input_file:
 	lowestELO = {}
 	lowestELO['score'] = 1200
 
-	for line_count, row in enumerate(csv_reader):
+	for line_count, row in enumerate(masterSheet):
 		if line_count == 0:
 			for name in row[1:]:
 				Elos[name] = 1200
@@ -58,6 +22,8 @@ with open(sys.argv[1]) as input_file:
 			Gamescore = {}
 			OldElos = {}
 			for index, score in enumerate(row[1:]):
+				if score == '':
+					score = 0
 				if int(score) >= 2:
 					Gamescore[playersArray[index]] = score
 					OldElos[playersArray[index]] = Elos[playersArray[index]]
@@ -73,7 +39,7 @@ with open(sys.argv[1]) as input_file:
 							status = 0
 						# print(f'{status} = status')
 						# K is constant for now
-						tempSC = CalcEloChange(Elos[player], OldElos[opp], 30, status)
+						tempSC = eloCalculator.CalcEloChange(Elos[player], OldElos[opp], 30, status)
 						# print(f'{player} {Elos[player]} scored {score}, Opponent {opp} {OldElos[opp]} scored {oppScore}, change to player {tempSC}')
 						scoreChange += tempSC
 				# print(f'{player} scoreChange = {scoreChange}')
@@ -112,3 +78,6 @@ with open(sys.argv[1]) as input_file:
 
 	for name in sorted(Elos, key=Elos.get, reverse=True):
 		print(f'{name}: {Elos[name]}')
+
+if __name__ == '__main__':
+    main()
